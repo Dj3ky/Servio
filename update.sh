0607a888a002f0133c7e77c5ee33a60799d4b19a
Dj3ky <101254571+Dj3ky@users.noreply.github.com>
2026-06-02 16:37:51 +0200

    fix

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ echo "==> Pulling latest changes..."
 git pull origin main
 
 echo "==> Installing dependencies..."
-PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 NODE_ENV=development npm install
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 NODE_ENV=development npm ci
 
 echo "==> Building..."
 NODE_ENV=development npm run build
